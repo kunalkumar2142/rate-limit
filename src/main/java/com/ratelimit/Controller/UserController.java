01daa4c5f784022c60758c31e184cbcbbf6e2054
Kunal Kumar <4002kunal88@gmail.com>
2026-07-07 16:31:14 +0530

    controller is created setuped  -7july 2026

---
+++ src/main/java/com/ratelimit/Controller/UserController.java
@@ -0,0 +1,18 @@
+package com.ratelimit.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/v1/user")
+    public String getUser() {
+        return "Hello Secure User";
+    }
+
+    @GetMapping("/v2/user")
+    public String getUserNotSecure() {
+        return "Hello Not Secure User";
+    }
+}
